546e8c9f54c702e530c30ef2ba2876fa33c2ee62
zghh <1069308575@qq.com>
2022-01-03 20:34:04 +0800

    Add sm9 curve pairing implement.

--- ecc/sm9_param.c
+++ ecc/sm9_param.c
@@ -20,6 +20,7 @@ struct sm9_param_s
     mpz_t q;    // Curve defined over F_q.
     mpz_t r;    // The order of the curve.
     mpz_t b;    // E: y^2 = x^3 + b
+    mpz_t t;    // Curve parameter
     mpz_t beta; // beta is a quadratic nonresidue in Fq, we use F_q^2 = F_q[sqrt(beta)]
     mpz_t alpha0, alpha1;
     // the polynomial x^6 + alpha0 + alpha1 sqrt(beta)
@@ -39,6 +40,7 @@ struct sm9_pairing_data_s
     element_t negalpha;
     element_t negalphainv;
     mpz_t tateexp;
+    mpz_t t, q;
 
     // for tate exponentiation speedup:
     // x^{q^k} for various k
@@ -53,6 +55,7 @@ static void sm9_clear(void *data)
     mpz_clear(fp->q);
     mpz_clear(fp->r);
     mpz_clear(fp->b);
+    mpz_clear(fp->t);
     mpz_clear(fp->beta);
     mpz_clear(fp->alpha0);
     mpz_clear(fp->alpha1);
@@ -66,266 +69,205 @@ static void sm9_out_str(FILE *stream, void *data)
     param_out_mpz(stream, "q", p->q);
     param_out_mpz(stream, "r", p->r);
     param_out_mpz(stream, "b", p->b);
+    param_out_mpz(stream, "b", p->t);
     param_out_mpz(stream, "beta", p->beta);
     param_out_mpz(stream, "alpha0", p->alpha0);
     param_out_mpz(stream, "alpha1", p->alpha1);
 }
 
-static void sm9_tryminusx(mpz_ptr q, mpz_ptr x)
+static void sm9_finalpow(element_t out) {}
+
+static void sm9_tangent(element_t out, element_t a, element_t c, sm9_pairing_data_ptr p)
 {
-    // 36x4 - 36x3 + 24x2 - 6x + 1
-    //= ((36(x - 1)x + 24)x - 6)x + 1
-    mpz_sub_ui(q, x, 1);
-    mpz_mul(q, q, x);
-    mpz_mul_ui(q, q, 36);
-    mpz_add_ui(q, q, 24);
-    mpz_mul(q, q, x);
-    mpz_sub_ui(q, q, 6);
-    mpz_mul(q, q, x);
-    mpz_add_ui(q, q, 1);
+    element_ptr ax, ay, cx, cy, cof0x, cof0y, out0, out3, out5;
+    element_t lamda, cof0, cof3, cof5, temp;
+
+    ax = element_x(a);
+    ay = element_y(a);
+    cx = element_x(c);
+    cy = element_y(c);
+
+    element_init_same_as(lamda, ax);
+    element_square(lamda, ax);
+    element_mul_si(lamda, lamda, 3);
+    element_init_same_as(temp, ay);
+    element_mul_si(temp, ay, 2);
+    element_div(lamda, lamda, temp);
+
+    element_init_same_as(cof3, ay);
+    element_set(cof3, ay);
+    element_mul(temp, lamda, ax);
+    element_sub(cof3, ay, temp);
+
+    element_init_same_as(cof5, lamda);
+    element_mul_zn(cof5, lamda, cx);
+
+    element_init_same_as(cof0, ax);
+    cof0x = element_x(cof0);
+    cof0y = element_y(cof0);
+    element_neg(cof0x, cy);
+    element_set0(cof0y);
+
+    out0 = out->field->item(out, 0);
+    out3 = out->field->item(out, 3);
+    out5 = out->field->item(out, 5);
+    element_set(out0, cof0);
+    element_mul(out3, cof3, p->negalphainv);
+    element_mul(out5, cof5, p->negalphainv);
+
+    element_clear(lamda);
+    element_clear(cof0);
+    element_clear(cof3);
+    element_clear(cof5);
+    element_clear(temp);
 }
 
-static void sm9_tryplusx(mpz_ptr q, mpz_ptr x)
+static void sm9_line(element_t out, element_t a, element_t b, element_t c, sm9_pairing_data_ptr p)
 {
-    // 36x4 + 36x3 + 24x2 + 6x + 1
-    //= ((36(x + 1)x + 24)x + 6)x + 1
-    mpz_add_ui(q, x, 1);
-    mpz_mul(q, q, x);
-    mpz_mul_ui(q, q, 36);
-    mpz_add_ui(q, q, 24);
-    mpz_mul(q, q, x);
-    mpz_add_ui(q, q, 6);
-    mpz_mul(q, q, x);
-    mpz_add_ui(q, q, 1);
+    element_ptr ax, ay, bx, by, cx, cy, cof0x, cof0y, out0, out3, out5;
+    element_t lamda, cof0, cof3, cof5, temp;
+
+    ax = element_x(a);
+    ay = element_y(a);
+    bx = element_x(b);
+    by = element_y(b);
+    cx = element_x(c);
+    cy = element_y(c);
+
+    element_init_same_as(lamda, ay);
+    element_sub(lamda, ay, by);
+    element_init_same_as(temp, ax);
+    element_sub(temp, ax, bx);
+    element_div(lamda, lamda, temp);
+
+    element_init_same_as(cof3, by);
+    element_set(cof3, by);
+    element_mul(temp, lamda, bx);
+    element_sub(cof3, by, temp);
+
+    element_init_same_as(cof5, lamda);
+    element_mul_zn(cof5, lamda, cx);
+
+    element_init_same_as(cof0, ax);
+    cof0x = element_x(cof0);
+    cof0y = element_y(cof0);
+    element_neg(cof0x, cy);
+    element_set0(cof0y);
+
+    out0 = out->field->item(out, 0);
+    out3 = out->field->item(out, 3);
+    out5 = out->field->item(out, 5);
+    element_set(out0, cof0);
+    element_mul(out3, cof3, p->negalphainv);
+    element_mul(out5, cof5, p->negalphainv);
+
+    element_clear(lamda);
+    element_clear(cof0);
+    element_clear(cof3);
+    element_clear(cof5);
+    element_clear(temp);
 }
 
-static void sm9_cc_miller_no_denom(element_t res, mpz_t q, element_t P,
-                                   element_ptr Qx, element_ptr Qy, element_t negalpha)
+static void sm9_frobenius(element_t out, element_t point, sm9_pairing_data_ptr p)
 {
-    int m;
-    element_t v;
-    element_t Z;
-    element_t a, b, c;
-    element_t t0;
-    element_t e0, e1;
-    element_ptr Zx, Zy;
-    const element_ptr Px = curve_x_coord(P);
-    const element_ptr Py = curve_y_coord(P);
+    element_t x, r, w, px, py;
+    mpz_t temp;
 
-#define do_term(i, j, k, flag)                                        \
-    {                                                                 \
-        element_ptr e2;                                               \
-        e2 = element_item(e0, i);                                     \
-        element_mul(e1, element_item(v, j), Qx);                      \
-        if (flag == 1)                                                \
-            element_mul(e1, e1, negalpha);                            \
-        element_mul(element_x(e1), element_x(e1), a);                 \
-        element_mul(element_y(e1), element_y(e1), a);                 \
-        element_mul(e2, element_item(v, k), Qy);                      \
-        element_mul(element_x(e2), element_x(e2), b);                 \
-        element_mul(element_y(e2), element_y(e2), b);                 \
-        element_add(e2, e2, e1);                                      \
-        if (flag == 2)                                                \
-            element_mul(e2, e2, negalpha);                            \
-        element_mul(element_x(e1), element_x(element_item(v, i)), c); \
-        element_mul(element_y(e1), element_y(element_item(v, i)), c); \
-        element_add(e2, e2, e1);                                      \
-    }
+    element_init_same_as(px, element_x(point));
+    element_set(px, element_x(point));
+    element_init_same_as(py, element_y(point));
+    element_set(py, element_y(point));
 
-// a, b, c lie in Fq
-// Qx, Qy lie in Fq^2
-// Qx is coefficient of x^4
-// Qy is coefficient of x^3
-//
-// computes v *= (a Qx x^4 + b Qy x^3 + c)
-//
-// recall x^6 = -alpha thus
-// x^4 (u0 + u1 x^1 + ... + u5 x^5) =
-// u0 x^4 + u1 x^5
-// - alpha u2 - alpha u3 x - alpha u4 x^2 - alpha u5 x^3
-// and
-// x^4 (u0 + u1 x^1 + ... + u5 x^5) =
-// u0 x^3 + u1 x^4 + u2 x^5
-// - alpha u3 - alpha u4 x - alpha u5 x^2
-#define f_miller_evalfn()    \
-    {                        \
-        do_term(0, 2, 3, 2); \
-        do_term(1, 3, 4, 2); \
-        do_term(2, 4, 5, 2); \
-        do_term(3, 5, 0, 1); \
-        do_term(4, 0, 1, 0); \
-        do_term(5, 1, 2, 0); \
-        element_set(v, e0);  \
-    }
-/*
-  element_ptr e1;
+    element_init(x, p->Fq2);
+    element_set0(element_x(x));
+    element_set1(element_y(x));
+    mpz_init(temp);
+    mpz_sub_ui(temp, p->q, 1);
+    mpz_divexact_ui(temp, temp, 6);
+    element_pow_mpz(x, x, temp);
 
-  e1 = element_item(e0, 4);
+    element_init_same_as(r, x);
+    element_invert(r, x);
+    element_init_same_as(w, r);
+    element_square(w, r);
 
-  element_mul(element_x(e1), element_x(Qx), a);
-  element_mul(element_y(e1), element_y(Qx), a);
+    element_neg(element_y(px), element_y(px));
+    element_mul(px, px, w);
 
-  e1 = element_item(e0, 3);
+    element_neg(element_y(py), element_y(py));
+    element_mul(py, py, w);
+    element_mul(py, py, r);
 
-  element_mul(element_x(e1), element_x(Qy), b);
-  element_mul(element_y(e1), element_y(Qy), b);
+    element_random(out);
+    element_set(element_x(out), px);
+    element_set(element_y(out), py);
 
-  element_set(element_x(element_item(e0, 0)), c);
-
-  element_mul(v, v, e0);
- */
-
-// a = -3 Zx^2 since cc->a is 0 for D = 3
-// b = 2 * Zy
-// c = -(2 Zy^2 + a Zx);
-#define do_tangent()             \
-    {                            \
-        element_square(a, Zx);   \
-        element_mul_si(a, a, 3); \
-        element_neg(a, a);       \
-                                 \
-        element_add(b, Zy, Zy);  \
-                                 \
-        element_mul(t0, b, Zy);  \
-        element_mul(c, a, Zx);   \
-        element_add(c, c, t0);   \
-        element_neg(c, c);       \
-                                 \
-        f_miller_evalfn();       \
-    }
-
-// a = -(B.y - A.y) / (B.x - A.x);
-// b = 1;
-// c = -(A.y + a * A.x);
-// but we'll multiply by B.x - A.x to avoid division
-#define do_line()               \
-    {                           \
-        element_sub(b, Px, Zx); \
-        element_sub(a, Zy, Py); \
-        element_mul(t0, b, Zy); \
-        element_mul(c, a, Zx);  \
-        element_add(c, c, t0);  \
-        element_neg(c, c);      \
-                                \
-        f_miller_evalfn();      \
-    }
-
-    element_init(a, Px->field);
-    element_init(b, a->field);
-    element_init(c, a->field);
-    element_init(t0, a->field);
-    element_init(e0, res->field);
-    element_init(e1, Qx->field);
-
-    element_init(v, res->field);
-    element_init(Z, P->field);
-
-    element_set(Z, P);
-    Zx = curve_x_coord(Z);
-    Zy = curve_y_coord(Z);
-
-    element_set1(v);
-    m = mpz_sizeinbase(q, 2) - 2;
-
-    // TODO: sliding NAF
-    for (;;)
-    {
-        do_tangent();
-
-        if (!m)
-            break;
-
-        element_double(Z, Z);
-        if (mpz_tstbit(q, m))
-        {
-            do_line();
-            element_add(Z, Z, P);
-        }
-        m--;
-        element_square(v, v);
-    }
-
-    element_set(res, v);
-
-    element_clear(v);
-    element_clear(Z);
-    element_clear(a);
-    element_clear(b);
-    element_clear(c);
-    element_clear(t0);
-    element_clear(e0);
-    element_clear(e1);
-#undef do_term
-#undef f_miller_evalfn
-#undef do_tangent
-#undef do_line
-}
-
-static void sm9_tateexp(element_t out)
-{
-    element_t x, y, epow;
-    sm9_pairing_data_ptr p = out->field->pairing->data;
-    element_init(x, p->Fq12);
-    element_init(y, p->Fq12);
-    element_init(epow, p->Fq2);
-
-#define qpower(e1, e)                                                 \
-    {                                                                 \
-        element_set(element_item(e1, 0), element_item(out, 0));       \
-        element_mul(element_item(e1, 1), element_item(out, 1), e);    \
-        element_square(epow, e);                                      \
-        element_mul(element_item(e1, 2), element_item(out, 2), epow); \
-        element_mul(epow, epow, e);                                   \
-        element_mul(element_item(e1, 3), element_item(out, 3), epow); \
-        element_mul(epow, epow, e);                                   \
-        element_mul(element_item(e1, 4), element_item(out, 4), epow); \
-        element_mul(epow, epow, e);                                   \
-        element_mul(element_item(e1, 5), element_item(out, 5), epow); \
-    }
-
-    qpower(y, p->xpowq8);
-    qpower(x, p->xpowq6);
-    element_mul(y, y, x);
-    qpower(x, p->xpowq2);
-    element_mul(x, x, out);
-    element_invert(x, x);
-    element_mul(out, y, x);
-
-    element_clear(epow);
     element_clear(x);
-    element_clear(y);
-    element_pow_mpz(out, out, p->tateexp);
-#undef qpower
-}
-
-static void sm9_finalpow(element_t out)
-{
-    sm9_tateexp(out->data);
+    element_clear(r);
+    element_clear(w);
+    element_clear(px);
+    element_clear(py);
+    mpz_clear(temp);
 }
 
 static void sm9_pairing(element_ptr out, element_ptr in1, element_ptr in2,
                         pairing_t pairing)
 {
-    element_ptr Qbase = in2;
-    element_t x, y;
     sm9_pairing_data_ptr p = pairing->data;
+    mpz_t a, temp;
+    int i;
+    element_t t, f, line, q1, q2, negq2;
 
-    element_init(x, p->Fq2);
-    element_init(y, p->Fq2);
-    // map from twist: (x, y) --> (v^-2 x, v^-3 y)
-    // where v is the sixth root used to construct the twist
-    // i.e. v^6 = -alpha
-    // thus v^-2 = -alpha^-1 v^4
-    // and  v^-3 = -alpha^-1 v^3
-    element_mul(x, curve_x_coord(Qbase), p->negalphainv);
-    element_mul(y, curve_y_coord(Qbase), p->negalphainv);
+    element_init(line, p->Fq12);
 
-    sm9_cc_miller_no_denom(out, pairing->r, in1, x, y, p->negalpha);
+    mpz_init(a);
+    mpz_mul_ui(a, p->t, 6);
+    mpz_add_ui(a, a, 2);
+    element_init_same_as(t, in2);
+    element_set(t, in2);
+    element_init(f, p->Fq12);
+    element_set1(f);
 
-    element_clear(x);
-    element_clear(y);
+    for (i = mpz_sizeinbase(a, 2) - 2; i >= 0; i--)
+    {
+        element_square(f, f);
+        sm9_tangent(line, t, in1, p);
+        element_mul(f, f, line);
+        element_add(t, t, t);
+        if (mpz_tstbit(a, i))
+        {
+            sm9_line(line, t, in2, in1, p);
+            element_mul(f, f, line);
+            element_add(t, t, in2);
+        }
+    }
 
-    sm9_tateexp(out);
+    element_init_same_as(q1, in2);
+    sm9_frobenius(q1, in2, p);
+    element_init_same_as(q2, q1);
+    sm9_frobenius(q2, q1, p);
+    sm9_line(line, t, q1, in1, p);
+    element_mul(f, f, line);
+    element_add(t, t, q1);
+    element_init_same_as(negq2, q2);
+    element_neg(negq2, q2);
+    sm9_line(line, t, negq2, in1, p);
+    element_mul(f, f, line);
+    element_sub(t, t, q2);
+    mpz_init(temp);
+    mpz_pow_ui(temp, p->q, 12);
+    mpz_sub_ui(temp, temp, 1);
+    mpz_divexact(temp, temp, pairing->r);
+    element_pow_mpz(out, f, temp);
+
+    mpz_clear(a);
+    mpz_clear(temp);
+    element_clear(t);
+    element_clear(f);
+    element_clear(line);
+    element_clear(q1);
+    element_clear(q2);
+    element_clear(negq2);
 }
 
 static void sm9_pairing_clear(pairing_t pairing)
@@ -345,6 +287,8 @@ static void sm9_pairing_clear(pairing_t pairing)
     field_clear(p->Fq2x);
     field_clear(p->Fq2);
     field_clear(p->Fq);
+    mpz_clear(p->t);
+    mpz_clear(p->q);
     pbc_free(p);
 
     mpz_clear(pairing->r);
@@ -464,6 +408,11 @@ static void sm9_init_pairing(pairing_t pairing, void *data)
     element_set(p->xpowq8, element_item(xpowq, 1));
 
     element_clear(xpowq);
+
+    mpz_init(p->t);
+    mpz_set(p->t, param->t);
+    mpz_init(p->q);
+    mpz_set(p->q, param->q);
 }
 
 static void sm9_init(pbc_param_ptr p)
@@ -478,6 +427,7 @@ static void sm9_init(pbc_param_ptr p)
     mpz_init(fp->q);
     mpz_init(fp->r);
     mpz_init(fp->b);
+    mpz_init(fp->t);
     mpz_init(fp->beta);
     mpz_init(fp->alpha0);
     mpz_init(fp->alpha1);
@@ -494,138 +444,9 @@ int pbc_param_init_sm9(pbc_param_ptr par, struct symtab_s *tab)
     err += lookup_mpz(p->q, tab, "q");
     err += lookup_mpz(p->r, tab, "r");
     err += lookup_mpz(p->b, tab, "b");
+    err += lookup_mpz(p->t, tab, "t");
     err += lookup_mpz(p->beta, tab, "beta");
     err += lookup_mpz(p->alpha0, tab, "alpha0");
     err += lookup_mpz(p->alpha1, tab, "alpha1");
     return err;
-}
-
-void pbc_param_init_sm9_gen(pbc_param_t p, int bits)
-{
-    sm9_init(p);
-    sm9_param_ptr fp = p->data;
-    // 36 is a 6-bit number
-    int xbit = (bits - 6) / 4;
-    // TODO: use binary search to find smallest appropriate x
-    mpz_t x, t;
-    mpz_ptr q = fp->q;
-    mpz_ptr r = fp->r;
-    mpz_ptr b = fp->b;
-    field_t Fq, Fq2, Fq2x;
-    element_t e1;
-    element_t f;
-    field_t c;
-    element_t P;
-
-    mpz_init(x);
-    mpz_init(t);
-    mpz_setbit(x, xbit);
-    for (;;)
-    {
-        mpz_mul(t, x, x);
-        mpz_mul_ui(t, t, 6);
-        mpz_add_ui(t, t, 1);
-        sm9_tryminusx(q, x);
-        mpz_sub(r, q, t);
-        mpz_add_ui(r, r, 1);
-        if (mpz_probab_prime_p(q, 10) && mpz_probab_prime_p(r, 10))
-            break;
-
-        sm9_tryplusx(q, x);
-        mpz_sub(r, q, t);
-        mpz_add_ui(r, r, 1);
-        if (mpz_probab_prime_p(q, 10) && mpz_probab_prime_p(r, 10))
-            break;
-
-        mpz_add_ui(x, x, 1);
-    }
-
-    field_init_fp(Fq, q);
-    element_init(e1, Fq);
-
-    for (;;)
-    {
-        element_random(e1);
-        field_init_curve_b(c, e1, r, NULL);
-        element_init(P, c);
-
-        element_random(P);
-
-        element_mul_mpz(P, P, r);
-        if (element_is0(P))
-            break;
-        element_clear(P);
-        field_clear(c);
-    }
-    element_to_mpz(b, e1);
-    element_clear(e1);
-    field_init_quadratic(Fq2, Fq);
-    element_to_mpz(fp->beta, field_get_nqr(Fq));
-    field_init_poly(Fq2x, Fq2);
-    element_init(f, Fq2x);
-
-    // Find an irreducible polynomial of the form f = x^6 + alpha.
-    // Call poly_set_coeff1() first so we can use element_item() for the other
-    // coefficients.
-    poly_set_coeff1(f, 6);
-    for (;;)
-    {
-        element_random(element_item(f, 0));
-        if (poly_is_irred(f))
-            break;
-    }
-
-    // extend F_q^2 using f = x^6 + alpha
-    // see if sextic twist contains a subgroup of order r
-    // if not, it's the wrong twist: replace alpha with alpha^5
-    {
-        field_t ctest;
-        element_t Ptest;
-        mpz_t z0, z1;
-        mpz_init(z0);
-        mpz_init(z1);
-        element_init(e1, Fq2);
-        element_set_mpz(e1, fp->b);
-        element_mul(e1, e1, element_item(f, 0));
-        element_neg(e1, e1);
-
-        field_init_curve_b(ctest, e1, r, NULL);
-        element_init(Ptest, ctest);
-        element_random(Ptest);
-
-        // I'm not sure what the #E'(F_q^2) is, but
-        // it definitely divides n_12 = #E(F_q^12). It contains a
-        // subgroup of order r if and only if
-        //(n_12 / r^2)P != O for some (in fact most) P in E'(F_q^6)
-        mpz_pow_ui(z0, q, 12);
-        mpz_add_ui(z0, z0, 1);
-        pbc_mpz_trace_n(z1, q, t, 12);
-        mpz_sub(z1, z0, z1);
-        mpz_mul(z0, r, r);
-        mpz_divexact(z1, z1, z0);
-
-        element_mul_mpz(Ptest, Ptest, z1);
-        if (element_is0(Ptest))
-        {
-            mpz_set_ui(z0, 5);
-            element_pow_mpz(element_item(f, 0), element_item(f, 0), z0);
-        }
-        element_clear(e1);
-        element_clear(Ptest);
-        field_clear(ctest);
-        mpz_clear(z0);
-        mpz_clear(z1);
-    }
-
-    element_to_mpz(fp->alpha0, element_x(element_item(f, 0)));
-    element_to_mpz(fp->alpha1, element_y(element_item(f, 0)));
-
-    element_clear(f);
-
-    field_clear(Fq2x);
-    field_clear(Fq2);
-    field_clear(Fq);
-
-    mpz_clear(t);
-    mpz_clear(x);
 }
